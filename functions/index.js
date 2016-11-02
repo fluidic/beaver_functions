5402103e55e7fba23e1f5ec395a4624f4f354349
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-11-02 12:51:08 +0900

    .

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,4 @@
-'use strict';
-
-const spawn = require('child_process').spawn;
-
-exports.beaver = function beaver(req, res) {
-    const child = spawn('./third_party/dart-linux-x64/dart',
-        [
-            'beaver.dart.snapshot',
-            req.path,
-            JSON.stringify(req.headers),
-            JSON.stringify(req.body)
-        ],
-        {
-            env: {LD_LIBRARY_PATH: './third_party/glibc-2.15-lib'}
-        }
-    );
-
-    child.stdout.on('data', (data) => {
-        data = String(data);
-        console.log(`stdout: ${data}`);
-        res.send(data);
-    });
-
-    child.stderr.on('data', (data) => {
-        data = String(data);
-        console.log(`stderr: ${data}`);
-    });
-
-    child.on('exit', (code) => {
-        console.log(`child process exited with code ${code}`);
-        res.status(200).end();
-    });
+exports.beaver = function helloGET (req, res) {
+    res.send('Hello World!');
 };
+
